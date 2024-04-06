e6626020aa72397b148632489e33adc77a699e4c
Siva Anand <sivaanand135@gmail.com>
2024-04-06 22:36:28 +0530

    Updated To do list.

--- Mini_tasks/To_do_list.py
+++ Mini_tasks/To_do_list.py
@@ -19,8 +19,24 @@ def view_todo_list(todo_list):
     print("To-Do List:")
     for index, task in enumerate(todo_list, start=1):
         print(f"{index}. {task}")
-    
+
+def remove_task(index,todo_list):
+    try:
+        todo_list.pop(index)
+        print("Task removed sucessfully")
+    except IndexError:
+        print("You entered an index which is not valid")
+
+def mark_completed(index,todo_list):
+    try:
+        task = todo_list[index]
+        todo_list[index] = f"[Completed] {task}" #f"[Completed] {task}"
+        print("Task marked as completed ")
+    except IndexError:
+        print("You entered an index which is not valid")
+
 def main():
+
     
     todo_list = []
     
@@ -29,16 +45,30 @@ def main():
         print("1. Add Task")
         print("2. Remove Task")
         print("3. View To-Do List")
-        print("4. Exit")
+        print("4. Mark task as completed")
+        print("5. Exit program")
 
-        choice = int(input("Enter your choice (1-4): ")) #Note that there was an issue that occured here because we forgot to convert the input to int.
+        choice = input("Enter your choice (1-4): ") #Note that there was an issue that occured here because we forgot to convert the input to int.
         
-        if choice == 1: 
+        if choice == '1': 
             task = input("Enter the task")
             add_task(task,todo_list)
+        
+        elif choice == '2':
+            index = int(input("Enter the input of the task you want to remove")) - 1
+            remove_task(index, todo_list)
             
-        elif choice == 3:
+        elif choice == '3':
             view_todo_list(todo_list)
+        
+        elif choice == '4':
+            view_todo_list(todo_list)
+            index = int(input("Enter the index of the task of the task you want to mark as completed.")) - 1
+            mark_completed(index, todo_list)
+            
+        elif choice == '5':
+            print("Exiting program...")
+            break
 
         else:
             print("Invalid choice! Please enter a number between 1 and 5.")
